d7246a0ae1e61e49197b0a9a97eceaaa6dd670e3
prakashVishwakarma <prksvsvkrma@gmail.com>
2023-08-03 10:36:25 +0530

    calendar api done with some other bug fixes

--- src/Component/Popups/AddEventPopup.jsx
+++ src/Component/Popups/AddEventPopup.jsx
@@ -2,90 +2,54 @@ import { useState } from 'react';
 import './New.css';
 import { RxCross2 } from "react-icons/rx";
 import { useDispatch } from 'react-redux';
-import { asyncThunkAddContractor, asyncThunkGetContractor } from '../../redux/createAsyncThunk';
+import { asyncThunkAddContractor, asyncThunkCreateTask, asyncThunkGetContractor } from '../../redux/createAsyncThunk';
 import { Button } from '@mui/material';
 
 
-function AddEventPopup({setShowPopup}) {
-  const [timesheetDetails, setTimesheetDetails] = useState({
-    date: '',
-    task: '',
-    time: '',
-  });
+// eslint-disable-next-line react/prop-types
+function AddEventPopup({ setShowPopup, dateState }) {
+
+  const [timesheetDetails, setTimesheetDetails] = useState({});
   const dispatch = useDispatch();
 
   const closePopup = () => {
     setShowPopup(false);
   };
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setTimesheetDetails((prevDetails) => ({
-      ...prevDetails,
-      [name]: value,
-    }));
-  };
+  const handleInputChange = (e) => { setTimesheetDetails((prev) => { prev[e.target.name] = e.target.value; return { ...prev } }) };
 
-  const handleAddContractor = () => {
+  const handleAddEvents = () => {
     const payload = {
-      "first_name": timesheetDetails.date,
-      "last_name": timesheetDetails.task,
-      "email": timesheetDetails.time
+      "date": dateState, ...timesheetDetails
+
+      // "task": timesheetDetails.task,
+      // "workingHour": timesheetDetails.workingHour
     }
-    dispatch(asyncThunkAddContractor(payload))
-    dispatch(asyncThunkGetContractor(1));
-    // setClients((prevClients) => [...prevClients, clientDetails]);
-    // setClientDetails({
-    //   name: '',
-    //   email: '',
-    //   phone: '',
-    // });
+    dispatch(asyncThunkCreateTask(payload))
+    // dispatch(asyncThunkGetContractor(1));
     closePopup();
   };
 
   return (
-        <div className="popup">
-        <div className="popup" onClick={closePopup}></div>
-          <div className="popup-inner">
-            <h2>Add Timesheet</h2>
-            
-            <form>
-              <label htmlFor="date">Date:</label>
-              <input
-                type="text"
-                id="date"
-                name="date"
-                value={timesheetDetails.date}
-                onChange={handleInputChange}
-              />
+    <div className="popup">
+      <div className="popup" onClick={closePopup}></div>
+      <div className="popup-inner">
+        <h2>Add Timesheet</h2>
+        <form>
+          <label htmlFor="date">Date:</label>
+          <input type="text" id="date" name="date" disabled value={dateState} />
 
-              <label htmlFor="task">Task:</label>
-              <input
-                type="text"
-                id="task"
-                name="task"
-                value={timesheetDetails.task}
-                onChange={handleInputChange}
-              />
+          <label htmlFor="task">Task:</label>
+          <input type="text" id="task" name="task" value={timesheetDetails.task} onChange={handleInputChange} />
 
-              <label htmlFor="time">Working Hours:</label>
-              <input
-                type="email"
-                id="time"
-                name="time"
-                value={timesheetDetails.time}
-                onChange={handleInputChange}
-              />
+          <label htmlFor="time">Working Hours:</label>
+          <input type="number" id="time" name="workingHour" value={timesheetDetails.workingHour} onChange={handleInputChange} />
 
-              <button type="button" className='add-button' onClick={handleAddContractor}>
-                ADD
-              </button>
-              <Button variant="outlined" color="error" sx={{mt:"15px"}} onClick={closePopup}>
-              Close
-            </Button>
-            </form>
-          </div>
-        </div>
+          <button disabled={timesheetDetails?.task && timesheetDetails?.workingHour ? false : true} style={{ cursor: !timesheetDetails?.task && !timesheetDetails?.workingHour && 'no-drop' }} type="button" className='add-button' onClick={handleAddEvents}>ADD</button>
+          <Button variant="outlined" color="error" sx={{ mt: "15px" }} onClick={closePopup}>Close</Button>
+        </form>
+      </div>
+    </div>
   );
 }
 
